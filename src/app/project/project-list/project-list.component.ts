fc31af511ccf0c7983dda37ad28ca4f7a7bac248
fsciuti <fsciuti@gmail.com>
2019-11-19 18:50:12 +0100

    feat: add ProjectListComponent

---
+++ src/app/project/project-list/project-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngptt-project-list',
+  templateUrl: './project-list.component.html',
+  styles: []
+})
+export class ProjectListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
