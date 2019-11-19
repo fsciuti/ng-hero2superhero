858b175c13c94ba047ca2d85c00554826cc79fb9
fsciuti <fsciuti@gmail.com>
2019-11-19 18:58:21 +0100

    feat: add static array data in ProjectList

--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '@app/models/project.model';
 
 @Component({
   selector: 'ngptt-project-list',
@@ -6,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProjectListComponent implements OnInit {
+  projects: Project[] = [
+    {
+      id: 1,
+      code: 'NHusYJl',
+      name: 'Progetto Alpha',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 1, 31),
+      end: new Date(2019, 3, 15),
+      priority: 'medium',
+      done: true,
+      tasks: []
+    },
+    {
+      id: 2,
+      code: 'SJieYKl',
+      name: 'Progetto Beta',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 3, 31),
+      end: new Date(2019, 6, 15),
+      priority: 'low',
+      done: true,
+      tasks: []
+    },
+    {
+      id: 3,
+      code: 'POjeGBs',
+      name: 'Progetto Gamma',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 8, 15),
+      priority: 'low',
+      done: false,
+      tasks: []
+    },
+  ];
 
   constructor() { }
 
