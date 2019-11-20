6b0c5b7335a70adde54f54703392820711fe0890
fsciuti <fsciuti@gmail.com>
2019-11-20 01:34:25 +0100

    feat: add ProjectService retry/catchError

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Project } from '@app/models/project.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +11,32 @@ export class ProjectService {
   constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get<Project[]>('http://localhost:8000/projects');
+    return this.http.get<Project[]>('http://localhost:8000/projects').pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get(id: number) {
-    return this.http.get<Project>(`http://localhost:8000/projects/${id}`);
+    return this.http.get<Project>(`http://localhost:8000/projects/${id}`).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
