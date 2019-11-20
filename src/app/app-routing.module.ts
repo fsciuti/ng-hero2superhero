2b1cd68c9f6de78e6772570521a68ab4a0d0a696
fsciuti <fsciuti@gmail.com>
2019-11-20 10:24:40 +0100

    fix: update AppRoutingModule rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponent } from './auth/login/login.component';
 import { ProjectContainerComponent } from './project/project-container/project-container.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
 
 const routes: Routes = [
     { path: 'projects', component: ProjectContainerComponent },
     { path: 'signin', component: LoginComponent },
+    { path: 'signout', component: LogoutComponent },
     { path: 'signup', component: RegisterComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full'},
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    { path: '', redirectTo: 'signin', pathMatch: 'full' },
+    { path: '**', redirectTo: 'signin', pathMatch: 'full' }
 ]
 
 @NgModule({
