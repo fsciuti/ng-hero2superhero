8776203a0a39ecd65014d035007aca6d1cabd0cc
fsciuti <fsciuti@gmail.com>
2019-11-20 14:56:00 +0100

    feat: update ProjectRoutingModule routes

--- src/app/project/project-routing.module.ts
+++ src/app/project/project-routing.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthenticationGuard } from '@app/auth/authentication.guard';
-import { ProjectContainerComponent } from './pages/project-container/project-container.component';
+import { ProjectComponent } from './pages/project.component';
+import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
+import { ProjectDashboardComponent } from './pages/project-dashboard/project-dashboard.component';
 
 
 const routes: Routes = [
-  { path: 'projects', component: ProjectContainerComponent, canActivate: [ AuthenticationGuard ] }
+  { 
+    path: 'projects', 
+    component: ProjectComponent,
+    children: [
+      { path: '', component: ProjectDashboardComponent, canActivate: [ AuthenticationGuard ] },
+      { path: ':id', component: ProjectDetailComponent, canActivate: [ AuthenticationGuard ] }
+    ]
+  }
 ];
 
 @NgModule({
