07539fd092cfd1d93cc3ef2227775acaf9f6b951
fsciuti <fsciuti@gmail.com>
2019-11-20 14:52:43 +0100

    feat: update ProjectDetail ActivatedRoute (draft)

--- src/app/project/components/project-detail/project-detail.component.ts
+++ src/app/project/components/project-detail/project-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap, tap, filter } from 'rxjs/operators';
+
+import { ProjectService } from '@app/project/project.service';
+import { Project } from '@app/models/project.model';
 
 @Component({
   selector: 'ngptt-project-detail',
@@ -9,9 +15,20 @@ export class ProjectDetailComponent implements OnInit {
   @Input() project;
   @Output() highlight = new EventEmitter();
 
-  constructor() { }
+  project$: Observable<Project>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.project$ = this.route.paramMap.pipe(
+      filter((params: ParamMap) => params.has('id')),
+      switchMap((params: ParamMap) =>
+        this.projectService.get(+params.get('id')))
+    );
   }
 
   onHighlight() {
