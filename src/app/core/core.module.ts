7dfdb08e4cf6b177f78a9accd69fbf4e3f68aef4
fsciuti <fsciuti@gmail.com>
2019-11-21 18:33:55 +0100

    feat: update CoreModule preventing reimport

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from './auth';
@@ -9,6 +9,12 @@ import { httpInterceptorProviders } from './auth';
     CommonModule,
     HttpClientModule
   ],
-  providers: [ httpInterceptorProviders ],
+  providers: [httpInterceptorProviders],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() core: CoreModule) {
+    if (core) {
+      throw new Error('Is already active!');
+    }
+  }
+}
