23156a0dc4c34e31a9d9357dbde33f3c01bd9016
fsciuti <fsciuti@gmail.com>
2019-11-22 17:02:22 +0100

    feat: add HomeComponent into LayoutModule

--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -1,2 +1,3 @@
 export * from './guide/guide.component';
 export * from './header/header.component';
+export * from './home/home.component';
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '@app/shared/shared.module';
+
 import { HeaderComponent } from './header/header.component';
 import { GuideComponent } from './guide/guide.component';
-import { SharedModule } from '@app/shared/shared.module';
-import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [ HeaderComponent, GuideComponent],
+  declarations: [ HeaderComponent, GuideComponent, HomeComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
-  exports: [HeaderComponent, GuideComponent]
+  exports: [HeaderComponent, GuideComponent, HomeComponent]
 })
 export class LayoutModule { }
